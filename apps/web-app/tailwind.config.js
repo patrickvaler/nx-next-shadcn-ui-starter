239ef8d3d1033953dfa38be0e43f842d5b0786ff
Patrick Valer <hello@patrickvaler.ch>
2023-11-15 02:26:26 +0100

    feat: add shared tailwind config

--- apps/web-app/tailwind.config.js
+++ apps/web-app/tailwind.config.js
@@ -1,17 +1,5 @@
-const { createGlobPatternsForDependencies } = require('@nx/react/tailwind');
-const { join } = require('path');
+const TailwindConfig = require('../../libs/ui-kit/util/src/tailwind/tailwind.config');
 
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    join(
-      __dirname,
-      '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}'
-    ),
-    ...createGlobPatternsForDependencies(__dirname),
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+  ...TailwindConfig,
 };
